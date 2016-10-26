99746e8ad1a45b3912b4b66f6a48ad3771efe751
Jean-pierre Vincent <jpvirtual@yahoo.com>
2016-10-26 12:26:37 +0200

    Namespace is now WPERF. Use ES6 instead of jQuery

--- tracktime.js
+++ tracktime.js
@@ -1,7 +1,7 @@
-!(function(namespace) {
-  'use strict';
-  var $ = window.$ || require('jquery');
-  var timers = {};
+/* globals performance */
+!(function (namespace) {
+  'use strict'
+  var timers = {}
   /*
   * Collect a timing value you calculated yourself (1.2s = 1200 milliseconds) :
   * namespace.tracktime(
@@ -15,92 +15,88 @@
       { category:'Page article',
         subcategory:'image principale', label:this.src)" />
   */
-  namespace.trackTime = function(params) {
+  namespace.trackTime = function (args) {
     // defaults
-    var defaults = {
-      category: 'Global', // {string} in GA, the category name
-      subcategory: '', // {string} in GA, the variable
-      timing: 0, // {number}, milliseconds : performance.now() by default
-      label: '-', // {string} very optional, allows for more details in the tracking (like the name of the resource tracked)
-      probability: 15, // {number}, percentage, between 0 and 100 : will limit the number of reports sent back to your backend
-      once: true, // {boolean}: set to false if you want to allow tracking multiple time the same triplet (cat/subcat/label)
-    };
-    params = $.extend(defaults, params);
+    var {
+      category = 'Global', // {string} in GA, the category name
+      subcategory = '', // {string} in GA, the variable
+      timing = 0, // {number}, milliseconds  = performance.now() by default
+      label = '-', // {string} very optional, allows for more details in the tracking (like the name of the resource tracked)
+      probability = 10, // {number}, percentage, between 0 and 100  = will limit the number of reports sent back to your backend
+      once = true // {boolean}: set to false if you want to allow tracking multiple time the same triplet (cat/subcat/label)
+    } = args
 
     // no value given, let's ask that to the browser
-    if (!params.timing && 'performance' in window && 'now' in performance) {
+    if (!timing && 'performance' in window && 'now' in performance) {
       // call performance.mark() - standard way to track time (for ex by WPT)
       if ('mark' in performance) {
-        performance.mark(params.category + ' - ' + params.subcategory);
+        performance.mark(category + ' - ' + subcategory)
       }
 
-      params.timing = performance.now();
+      timing = performance.now()
       // more than 1 hour ? some weird browsers (maxthon, some versions of IE 11) report crazy times ( > 10 hours)
       // let ignore them
-      if (params.timing > 3600000) {
-        return false;
+      if (timing > 3600000) {
+        return false
       }
     }
 
-    if (!params.category || !params.subcategory) {
-      throw 'mandatory parameters : "category" and "subcategory"';
+    if (!category || !subcategory) {
+      throw 'mandatory parameters : "category" and "subcategory"'
     }
 
-    if (params.once && params.category + params.subcategory + params.label in timers)
-      return false;
-    timers[params.category + params.subcategory + params.label] = true;
+    if (once && category + subcategory + label in timers)
+      return false
+    timers[category + subcategory + label] = true
 
-    params.timing = Math.round(params.timing);
-    if (params.timing) {
+    timing = Math.round(timing)
+    if (timing) {
       // log
-      "console" in window && console.log('timings', params.category, params.subcategory, params.timing);
+      'console' in window && console.info('Timings', category, subcategory, timing)
       // threshold
-      if(Math.random()*100 > params.probability)
-        return true;
+      if (Math.random() * 100 > probability)
+        return true
       // send to google analytics universal (https://developers.google.com/analytics/devguides/collection/analyticsjs/user-timings)
       // dont forget to set a siteSpeedSampleRate to 100% https://developers.google.com/analytics/devguides/collection/analyticsjs/field-reference#siteSpeedSampleRate
-      if ('ga' in window) {
-          ga('send', 'timing', params.category, params.subcategory, params.timing, params.label);
+      if ('ga' in window && timing) {
+          ga('send', 'timing', category, subcategory, timing, label)
       }
       // uncomment this line if you need ALL the stats, even if GA is not there yet
       // window._gaq = window._gaq || [];
       // send to google analytics, legacy version (https://developers.google.com/analytics/devguides/collection/gajs/methods/gaJSApiUserTiming)
-      if ('_gaq' in window) {
-        _gaq.push(["_trackTiming", params.category, params.subcategory, params.timing, params.label, 100]);
+      if ('_gaq' in window && timing) {
+        _gaq.push(["_trackTiming", category, subcategory, timing, label, 100]);
       }
+
       // Send to webperf.io (basilic) http://support.basilic.io/getting_started/user-timings.html
-      if('FOGLIO' in window){
-        var name = params.category + ' '+ params.subcategory +' '+ params.label
+      if ('FOGLIO' in window) {
+        var name = category + ' ' + subcategory + ' ' + label
         name = name.replace(/^[a-zA-Z0-9_-]/g, '_')
         FOGLIO.q('mark', {
         // console.log('mark', {
           name: name,
-          value: performance.timing.navigationStart + params.timing
+          value: performance.timing.navigationStart + timing
         })
       }
     }
-
-  };
+  }
 
   // auto-execute collecting the first paint time (only if requestTracker library is there : https://github.com/jpvincent/requestTracker )
-  "PT" in window && PT.getFirstPaintTime(function(timeToFirstPaint) {
+  'WPERF' in window && namespace.getFirstPaintTime(function (timeToFirstPaint) {
     namespace.trackTime(
-      {category:"Page Load", subcategory:"First Paint", timing:timeToFirstPaint});
-
-  });
+      {category: 'Page Load', subcategory: 'First Paint', timing: timeToFirstPaint})
+  })
   // auto-record the page connection time (mainly latency of the user)
-  "performance" in window 
-    && "timing" in window.performance 
-    && performance.timing.connectEnd > 0 
-    && performance.timing.connectStart > 0 
-    && performance.timing.connectEnd - performance.timing.connectStart > 0 
-    && namespace.trackTime({category :"Page Load", subcategory:"connection time", 
-        timing: (performance.timing.connectEnd - performance.timing.connectStart)} );
+  'performance' in window &&
+    'timing' in window.performance &&
+    performance.timing.connectEnd > 0 &&
+    performance.timing.connectStart > 0 &&
+    performance.timing.connectEnd - performance.timing.connectStart > 0 &&
+    namespace.trackTime({category: 'Page Load', subcategory: 'connection time',
+      timing: (performance.timing.connectEnd - performance.timing.connectStart)})
+})(window.WPERF = window.WPERF || {})
 
-})(window);
-
-
-// for this lib to work in commonJS environments (like nodeJS)
-if (typeof module === "object" && typeof module.exports === "object") {
-  module.exports = window.trackTime;
+// for this lib to work in commonJS environments
+if (typeof module === 'object' && typeof module.exports === 'object') {
+  module.exports = window.WPERF.trackTime
 }
